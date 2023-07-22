dc394a4ec0f05b70e017f367dbcfb282f03b99c2
Chuck <charles@warp.inc>
2023-07-22 12:29:56 -0700

    docs and remove unused const

--- crates/explorer-client/src/endpoints/mod.rs
+++ crates/explorer-client/src/endpoints/mod.rs
@@ -6,13 +6,30 @@ use tracing::Instrument;
 
 use crate::Client;
 
+/// A call to the [Wormhole Explorer API].
+///
+/// [Wormhole Explorer API]: https://doc.wormscan.io/
 pub trait ApiCall: Send + Sync + Debug {
+    /// We expect the API to return some data of this type in a json object of
+    /// the following shape:
+    ///
+    /// ```ignore
+    /// {
+    ///   "data": Self::Return,
+    ///   "pagination": { "next": "" },
+    /// }
+    /// ```
     type Return: serde::de::DeserializeOwned;
 
-    const METHOD: reqwest::Method = reqwest::Method::GET;
-
+    /// Return the endpoint to which this request should be sent.
+    ///
+    /// This is typically computed from the request contents and a constant
+    /// stem. No POST requests are currently supported
     fn endpoint(&self) -> String;
 
+    /// Send the request over an API client.
+    ///
+    /// Do not override this method unless you know what you're doing.
     #[tracing::instrument(skip(self, client), fields(url, uuid))]
     fn send(&self, client: &Client) -> Pin<Box<dyn Future<Output = crate::Result<Self::Return>>>> {
         // UUID ensures event lifecycles can be tracked
