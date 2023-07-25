cac7ab01fa6dc5c150c7d133f47f771183669fcb
A5 Pickle <a5-pickle@users.noreply.github.com>
2023-07-24 23:05:36 -0400

    vaas: rename; remove encoded string

--- crates/vaas/src/protocol/encoded_types.rs
+++ crates/vaas/src/protocol/encoded_types.rs
@@ -1,4 +1,4 @@
-use alloy_primitives::{ruint::UintTryFrom, FixedBytes, U256, U8};
+use alloy_primitives::{ruint::UintTryFrom, U256, U8};
 
 use crate::{Readable, Writeable};
 
@@ -22,7 +22,7 @@ impl EncodedAmount {
     /// Create a new encoded amount by normalizing a raw amount sdjusted by an
     /// asset's decimals only if the decimals exceed the maximum allowed (8)
     /// for encoding.
-    pub fn from_raw(amount: U256, decimals: u8) -> Self {
+    pub fn norm(amount: U256, decimals: u8) -> Self {
         if decimals <= MAX_DECIMALS {
             Self(amount)
         } else {
@@ -32,7 +32,7 @@ impl EncodedAmount {
 
     /// Convert an encoded amount back to a raw amount by scaling it by its
     /// decimals if the decimals eceed the maximum allowed (8) from encoding.
-    pub fn to_raw(self, decimals: u8) -> U256 {
+    pub fn denorm(self, decimals: u8) -> U256 {
         if decimals <= MAX_DECIMALS {
             self.0
         } else {
@@ -43,7 +43,7 @@ impl EncodedAmount {
     /// Convert an encoded amount back to a raw amount by scaling it by its
     /// decimals if the decimals eceed the maximum allowed (8) from encoding.
     /// This method will return `None` if the raw amount overflows 32 bytes.
-    pub fn checked_to_raw(self, decimals: u8) -> Option<U256> {
+    pub fn checked_denorm(self, decimals: u8) -> Option<U256> {
         if decimals <= MAX_DECIMALS {
             Some(self.0)
         } else {
@@ -87,62 +87,6 @@ impl Writeable for EncodedAmount {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct EncodedString(pub FixedBytes<32>);
-
-impl From<String> for EncodedString {
-    fn from(value: String) -> Self {
-        let mut bytes = FixedBytes::<32>::default();
-        if value.len() > 32 {
-            bytes.copy_from_slice(&value.as_bytes()[..32]);
-        } else {
-            bytes[..value.len()].copy_from_slice(value.as_bytes());
-        }
-        Self(bytes)
-    }
-}
-
-impl From<EncodedString> for String {
-    fn from(value: EncodedString) -> Self {
-        use bstr::ByteSlice;
-        value
-            .0
-            .chars()
-            .filter(|&c| c != '\u{FFFD}')
-            .collect::<Vec<char>>()
-            .iter()
-            .collect::<String>()
-            .trim_end_matches(char::from(0))
-            .to_string()
-    }
-}
-
-impl Readable for EncodedString {
-    const SIZE: Option<usize> = Some(32);
-
-    fn read<R>(reader: &mut R) -> io::Result<Self>
-    where
-        Self: Sized,
-        R: io::Read,
-    {
-        Ok(Self(Readable::read(reader)?))
-    }
-}
-
-impl Writeable for EncodedString {
-    fn written_size(&self) -> usize {
-        <Self as Readable>::SIZE.unwrap()
-    }
-
-    fn write<W>(&self, writer: &mut W) -> io::Result<()>
-    where
-        Self: Sized,
-        W: io::Write,
-    {
-        self.0.write(writer)
-    }
-}
-
 fn pow10(power: u8) -> U256 {
     TEN.pow(U256::from(U8::from(power)))
 }
@@ -153,7 +97,7 @@ mod test {
 
     const TRIAL_AMOUNTS: [U256; 3] = [
         U256::ZERO,
-        U256::from_limbs([0, 0, 0, 0xffffffffffffffff]),
+        U256::from_limbs([0xffffffffffffffff, 0, 0, 0]),
         U256::MAX,
     ];
 
@@ -162,9 +106,9 @@ mod test {
         const DECIMALS: u8 = 7;
 
         for &amount in TRIAL_AMOUNTS.iter() {
-            let normalized = EncodedAmount::from_raw(amount, DECIMALS);
+            let normalized = EncodedAmount::norm(amount, DECIMALS);
             assert_eq!(normalized.0, amount);
-            let recovered = normalized.to_raw(DECIMALS);
+            let recovered = normalized.denorm(DECIMALS);
             assert_eq!(recovered, amount);
         }
     }
@@ -174,9 +118,9 @@ mod test {
         const DECIMALS: u8 = 8;
 
         for &amount in TRIAL_AMOUNTS.iter() {
-            let normalized = EncodedAmount::from_raw(amount, DECIMALS);
+            let normalized = EncodedAmount::norm(amount, DECIMALS);
             assert_eq!(normalized.0, amount);
-            let recovered = normalized.to_raw(DECIMALS);
+            let recovered = normalized.denorm(DECIMALS);
             assert_eq!(recovered, amount);
         }
     }
@@ -186,58 +130,18 @@ mod test {
         const DECIMALS: u8 = 9;
 
         for &amount in TRIAL_AMOUNTS.iter() {
-            let normalized = EncodedAmount::from_raw(amount, DECIMALS);
+            let normalized = EncodedAmount::norm(amount, DECIMALS);
             assert_eq!(normalized.0, amount.wrapping_div(TEN));
 
             // Recovered amount will be truncated.
-            let recovered = normalized.to_raw(DECIMALS);
+            let recovered = normalized.denorm(DECIMALS);
             assert_eq!(recovered, amount.wrapping_div(TEN).wrapping_mul(TEN));
         }
     }
 
     #[test]
     fn normalized_amount_too_large() {
-        let recovered = EncodedAmount::from(U256::MAX).checked_to_raw(9);
+        let recovered = EncodedAmount::from(U256::MAX).checked_denorm(9);
         assert_eq!(recovered, None);
-    }
-
-    #[test]
-    fn unicode_truncation_empty() {
-        let input = String::new();
-        let converted = EncodedString::from(input);
-        let recovered = String::from(converted);
-        assert_eq!(recovered, String::new());
-    }
-
-    #[test]
-    fn unicode_truncation_small() {
-        let input = String::from("🔥");
-        let converted = EncodedString::from(input);
-        let recovered = String::from(converted);
-        assert_eq!(recovered, String::from("🔥"));
-    }
-
-    #[test]
-    fn unicode_truncation_exact() {
-        let input = String::from("🔥🔥🔥🔥🔥🔥🔥🔥");
-        let converted = EncodedString::from(input);
-        let recovered = String::from(converted);
-        assert_eq!(recovered, String::from("🔥🔥🔥🔥🔥🔥🔥🔥"));
-    }
-
-    #[test]
-    fn unicode_truncation_large() {
-        let input = String::from("🔥🔥🔥🔥🔥🔥🔥🔥🔥");
-        let converted = EncodedString::from(input);
-        let recovered = String::from(converted);
-        assert_eq!(recovered, String::from("🔥🔥🔥🔥🔥🔥🔥🔥"));
-    }
-
-    #[test]
-    fn unicode_truncation_partial_overflow() {
-        let input = String::from("0000000000000000000000000000000🔥");
-        let converted = EncodedString::from(input);
-        let recovered = String::from(converted);
-        assert_eq!(recovered, String::from("0000000000000000000000000000000"));
     }
 }
