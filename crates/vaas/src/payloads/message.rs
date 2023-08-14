33271a4dde11164cc1c96b1468400140e2360f93
Chuck <charles@warp.inc>
2023-08-14 15:48:29 -0700

    new message format with prefix

--- crates/vaas/src/payloads/message.rs
+++ crates/vaas/src/payloads/message.rs
@@ -3,17 +3,18 @@ use crate::{Readable, Writeable};
 use std::io;
 
 impl crate::payloads::TypePrefixedPayload for Message {
-    const TYPE: Option<u8> = None;
+    const TYPE: Option<u8> = Some(0xbb);
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Message {
     pub(crate) version: u8,
+    pub(crate) message_ty: u8,
     pub(crate) index: u64,
     pub(crate) target_chain: u16,
-    pub(crate) sender: Vec<u8>,
     pub(crate) target: Vec<u8>,
-    pub(crate) message: Vec<u8>,
+    pub(crate) sender: Vec<u8>,
+    pub(crate) body: Vec<u8>,
 }
 
 impl Readable for Message {
@@ -24,28 +25,32 @@ impl Readable for Message {
         R: io::Read,
     {
         let version = u8::read(reader)?;
+        let message_ty = u8::read(reader)?;
         let index = u64::read(reader)?;
         let target_chain = u16::read(reader)?;
 
-        let sender_len = u16::read(reader)?;
-        let mut sender = vec![0u8; sender_len as usize];
-        reader.read_exact(sender.as_mut_slice())?;
+        dbg!("a");
 
         let target_len = u16::read(reader)?;
         let mut target = vec![0u8; target_len as usize];
         reader.read_exact(&mut target)?;
 
-        let message_len = u16::read(reader)?;
-        let mut message = vec![0u8; message_len as usize];
-        reader.read_exact(&mut message)?;
+        let sender_len = u16::read(reader)?;
+        let mut sender = vec![0u8; sender_len as usize];
+        reader.read_exact(sender.as_mut_slice())?;
+
+        let body_len = u16::read(reader)?;
+        let mut body = vec![0u8; body_len as usize];
+        reader.read_exact(&mut body)?;
 
         Ok(Self {
             version,
+            message_ty,
             index,
             target_chain,
             sender,
             target,
-            message,
+            body,
         })
     }
 }
@@ -56,18 +61,44 @@ impl Writeable for Message {
         W: io::Write,
     {
         self.version.write(writer)?;
+        self.message_ty.write(writer)?;
         self.index.write(writer)?;
         self.target_chain.write(writer)?;
-        (self.sender.len() as u16).write(writer)?;
-        writer.write_all(&self.sender)?;
         (self.target.len() as u16).write(writer)?;
         writer.write_all(&self.target)?;
-        (self.message.len() as u16).write(writer)?;
-        writer.write_all(&self.message)?;
+        (self.sender.len() as u16).write(writer)?;
+        writer.write_all(&self.sender)?;
+        (self.body.len() as u16).write(writer)?;
+        writer.write_all(&self.body)?;
         Ok(())
     }
 
     fn written_size(&self) -> usize {
-        1 + 8 + 2 + 2 + self.sender.len() + 2 + self.target.len() + 2 + self.message.len()
+        1 + 8 + 2 + 2 + self.sender.len() + 2 + self.target.len() + 2 + self.body.len()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use hex_literal::hex;
+
+    use super::*;
+    #[test]
+    fn it_roundtrips() {
+        let message = hex!(
+            "0000000000000000000012340002567800147fa9385be102ac3eac297483dd6233d62b3e149600029abc"
+        );
+
+        let message = Message::read(&mut &message[..]).unwrap();
+        dbg!(&message);
+
+        assert_eq!(message.index, 0);
+        assert_eq!(message.target_chain, 0x1234);
+        assert_eq!(message.target, hex!("5678"));
+        assert_eq!(
+            message.sender,
+            hex!("7fa9385be102ac3eac297483dd6233d62b3e1496")
+        );
+        assert_eq!(message.body, hex!("9abc"));
     }
 }
