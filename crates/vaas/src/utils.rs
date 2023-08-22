06627289daff6ec2f91121c3939c92406cd8d7c9
A5 Pickle <5342825+a5-pickle@users.noreply.github.com>
2023-08-22 15:17:22 -0500

    vaas: remove anchor feature (#36)
    Co-authored-by: A5 Pickle <a5-pickle@users.noreply.github.com>

--- crates/vaas/src/utils.rs
+++ crates/vaas/src/utils.rs
@@ -1,18 +1,9 @@
 use alloy_primitives::FixedBytes;
 
-#[cfg(feature = "anchor")]
-fn anchor_keccak(buf: &[u8]) -> FixedBytes<32> {
-    anchor_lang::solana_program::keccak::hash(buf).0.into()
-}
-
 /// Simple keccak256 hash with configurable backend.
 #[inline]
 pub fn keccak256(buf: impl AsRef<[u8]>) -> FixedBytes<32> {
-    #[cfg(not(feature = "anchor"))]
-    return alloy_primitives::keccak256(buf);
-
-    #[cfg(feature = "anchor")]
-    anchor_keccak(buf.as_ref())
+    alloy_primitives::keccak256(buf)
 }
 
 /// Return the number of guardians to reach quorum.
