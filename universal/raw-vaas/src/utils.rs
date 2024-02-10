472345b10cc202a86c4c19145b274ca1b6f2edf9
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-02-09 23:54:19 -0600

    raw-vaas: remove cfg features

--- universal/raw-vaas/src/utils.rs
+++ universal/raw-vaas/src/utils.rs
@@ -1,10 +1,3 @@
-/// Simple keccak256 hash with configurable backend.
-#[cfg(all(feature = "off-chain", not(feature = "on-chain")))]
-#[inline]
-pub fn keccak256(buf: impl AsRef<[u8]>) -> [u8; 32] {
-    alloy_primitives::keccak256(buf).into()
-}
-
 /// Return the number of guardians to reach quorum.
 #[inline]
 pub fn quorum(n: usize) -> usize {
