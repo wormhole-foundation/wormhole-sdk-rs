d62d415054d701f5695ba55571cf0b7e4dad2779
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-02-11 13:51:57 -0600

    solana/utils: fix result for feature

--- solana/utils/src/account_info/mod.rs
+++ solana/utils/src/account_info/mod.rs
@@ -1,10 +1,17 @@
-use solana_program::{account_info::AccountInfo, program_error::ProgramError};
+#![allow(clippy::result_large_err)]
+
+use solana_program::account_info::AccountInfo;
+
+cfg_if::cfg_if!(
+    if #[cfg(feature = "anchor")] {
+        type FeatureResult = anchor_lang::Result<()>;
+    } else {
+        type FeatureResult = Result<(), solana_program::program_error::ProgramError>;
+    }
+);
 
 /// Close an account by transferring all its lamports to another account.
-pub fn close_account(
-    info: &AccountInfo,
-    sol_destination: &AccountInfo,
-) -> Result<(), ProgramError> {
+pub fn close_account(info: &AccountInfo, sol_destination: &AccountInfo) -> FeatureResult {
     // Transfer tokens from the account to the sol_destination.
     let dest_starting_lamports = sol_destination.lamports();
     **sol_destination.lamports.borrow_mut() =
