6db9672b88865296909969d3348cd0f17035deee
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-02-12 12:45:56 -0600

    solana: invoke_with_context

--- solana/utils/src/cpi/bpf_loader_upgradeable.rs
+++ solana/utils/src/cpi/bpf_loader_upgradeable.rs
@@ -19,21 +19,20 @@ pub use __private::{
 
 mod __private {
     use super::*;
+    use crate::cpi::invoke_with_context;
 
     pub fn set_upgrade_authority<'info>(
         ctx: CpiContext<'_, '_, '_, 'info, SetUpgradeAuthority<'info>>,
         program_id: &Pubkey,
     ) -> Result<()> {
-        solana_program::program::invoke_signed(
+        invoke_with_context(
             &bpf_loader_upgradeable::set_upgrade_authority(
                 program_id,
                 ctx.accounts.current_authority.key,
                 ctx.accounts.new_authority.as_ref().map(|a| a.key),
             ),
-            &ctx.to_account_infos(),
-            ctx.signer_seeds,
+            ctx,
         )
-        .map_err(Into::into)
     }
 
     pub struct SetUpgradeAuthority<'info> {
@@ -76,16 +75,14 @@ mod __private {
         ctx: CpiContext<'_, '_, '_, 'info, SetUpgradeAuthorityChecked<'info>>,
         program_id: &Pubkey,
     ) -> Result<()> {
-        solana_program::program::invoke_signed(
+        invoke_with_context(
             &bpf_loader_upgradeable::set_upgrade_authority_checked(
                 program_id,
                 ctx.accounts.current_authority.key,
                 ctx.accounts.new_authority.key,
             ),
-            &ctx.to_account_infos(),
-            ctx.signer_seeds,
+            ctx,
         )
-        .map_err(Into::into)
     }
 
     #[derive(Accounts)]
@@ -98,16 +95,14 @@ mod __private {
     pub fn set_buffer_authority<'info>(
         ctx: CpiContext<'_, '_, '_, 'info, SetBufferAuthority<'info>>,
     ) -> Result<()> {
-        solana_program::program::invoke_signed(
+        invoke_with_context(
             &bpf_loader_upgradeable::set_buffer_authority(
                 ctx.accounts.buffer.key,
                 ctx.accounts.current_authority.key,
                 ctx.accounts.new_authority.key,
             ),
-            &ctx.to_account_infos(),
-            ctx.signer_seeds,
+            ctx,
         )
-        .map_err(Into::into)
     }
 
     #[derive(Accounts)]
@@ -120,16 +115,14 @@ mod __private {
     pub fn set_buffer_authority_checked<'info>(
         ctx: CpiContext<'_, '_, '_, 'info, SetBufferAuthorityChecked<'info>>,
     ) -> Result<()> {
-        solana_program::program::invoke_signed(
+        invoke_with_context(
             &bpf_loader_upgradeable::set_buffer_authority_checked(
                 ctx.accounts.buffer.key,
                 ctx.accounts.current_authority.key,
                 ctx.accounts.new_authority.key,
             ),
-            &ctx.to_account_infos(),
-            ctx.signer_seeds,
+            ctx,
         )
-        .map_err(Into::into)
     }
 
     #[derive(Accounts)]
@@ -140,17 +133,15 @@ mod __private {
     }
 
     pub fn upgrade<'info>(ctx: CpiContext<'_, '_, '_, 'info, Upgrade<'info>>) -> Result<()> {
-        solana_program::program::invoke_signed(
+        invoke_with_context(
             &bpf_loader_upgradeable::upgrade(
                 ctx.accounts.program.key,
                 ctx.accounts.buffer.key,
                 ctx.accounts.authority.key,
                 ctx.accounts.spill.key,
             ),
-            &ctx.to_account_infos(),
-            ctx.signer_seeds,
+            ctx,
         )
-        .map_err(Into::into)
     }
 
     #[derive(Accounts)]
--- solana/utils/src/cpi/mod.rs
+++ solana/utils/src/cpi/mod.rs
@@ -3,3 +3,44 @@
 pub mod bpf_loader_upgradeable;
 
 pub mod system_program;
+
+use anchor_lang::{prelude::*, solana_program::instruction::Instruction};
+
+pub fn invoke_data_with_context<'info, A, T>(
+    data: T,
+    ctx: CpiContext<'_, '_, '_, 'info, A>,
+) -> Result<()>
+where
+    A: ToAccountMetas + ToAccountInfos<'info>,
+    T: AnchorSerialize,
+{
+    invoke_raw_data_with_context(data.try_to_vec()?, ctx)
+}
+
+pub fn invoke_raw_data_with_context<'info, A>(
+    data: Vec<u8>,
+    ctx: CpiContext<'_, '_, '_, 'info, A>,
+) -> Result<()>
+where
+    A: ToAccountMetas + ToAccountInfos<'info>,
+{
+    invoke_with_context(
+        &Instruction {
+            program_id: ctx.program.key(),
+            data,
+            accounts: ctx.to_account_metas(None),
+        },
+        ctx,
+    )
+}
+
+pub fn invoke_with_context<'info, A>(
+    ix: &Instruction,
+    ctx: CpiContext<'_, '_, '_, 'info, A>,
+) -> Result<()>
+where
+    A: ToAccountMetas + ToAccountInfos<'info>,
+{
+    solana_program::program::invoke_signed(ix, &ctx.to_account_infos(), ctx.signer_seeds)
+        .map_err(Into::into)
+}
