48628e46946050b3a2312a4104f7939b19de78b4
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-04-30 16:49:34 -0500

    move ruint to workspace

--- cargo-publish.sh
+++ cargo-publish.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 cargo publish -p wormhole-io $@
-cargo publish -p wormhole-raw-vaas $@
+cargo publish -p wormhole-raw-vaas --features ruint $@
 cargo publish -p wormhole-solana-consts --features mainnet $@
 cargo publish -p wormhole-solana-utils --features anchor $@
 cargo publish -p wormhole-solana-vaas --features anchor,mainnet $@
